1c84e1797ae7d8f2314c664dc24e9f7a83e6c7e2
Ankit Kumar <91867197+Ankitkmm@users.noreply.github.com>
2023-05-21 17:01:19 +0530

    Create Removing Digits2.cpp

---
+++ Removing Digits2.cpp
@@ -0,0 +1,13 @@
+  void ma1n (){
+	   	  int n;
+	   	  cin >> n;
+	   	  vector<int> dp(n+1,INT_MAX);
+	   	  dp[0] = 0;
+	   	  for(int i = 0; i<=n; ++i){
+	   	    for(char c : to_string(i)){
+	   	    	dp[i] = min(dp[i], dp[i-(c-'0')] + 1);
+	   	    }
+	   	  }
+	   	  cout << dp[n] << n1;
+          }
+  
